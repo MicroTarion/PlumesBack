12b7d65aec377fa6e7123496563a0a5c88023060
Jason Saint-Germain <jasonsaintgermain@gmail.com>
2024-02-10 15:32:36 +0100

    test

--- plumes-projet-back/server.js
+++ plumes-projet-back/server.js
@@ -10,9 +10,16 @@ const router = require(path.join(__dirname, 'app', 'routes.js'));
 
 router(app);
 
+app.get('/api', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
 app.listen(process.env.PORT_HTTP, () => {
   if (process.env.APP_ENV == 'dev') {
     console.log(`Server is running on ${process.env.HTTP_SERVER}`);
   }
 });
+
+
